bfb98259d77e23e9a70a4c95f92f82d4a507b7e0
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-03-22 22:23:33 +0900

    chap03学習完了

---
+++ ch03/test_npm/index.js
@@ -0,0 +1,9 @@
+const { request } = require("undici");
+
+request("https://www.yahoo.co.jp")
+  .then((res) => {
+    return res.body.text();
+  })
+  .then((body) => {
+    console.log(body);
+  });
