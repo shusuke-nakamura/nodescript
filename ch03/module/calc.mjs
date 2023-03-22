4b41e5881e6581c6d179b8d8b244fdfd54cb4ed6
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-03-22 20:53:52 +0900

    リスト3.12まで学習完了

---
+++ ch03/module/calc.mjs
@@ -0,0 +1,9 @@
+export const num = 1;
+
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
---
+++ ch03/module/index.mjs
@@ -0,0 +1,9 @@
+import { num, add, sub } from "./calc.mjs";
+
+console.log(num);
+
+let res = add(3, 1);
+console.log(res);
+
+res = sub(3, 1);
+console.log(res);
